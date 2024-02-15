966d683c6911f2249ac756d3971130f27cab9bf8
Rishabh Shukla <rs515666@gmail.com>
2024-02-15 10:40:08 +0530

    Added new files

---
+++ Radio Button.java
@@ -0,0 +1,49 @@
+import java.awt.*;
+import javax.swing.*;
+
+class RadioButton
+{
+    public static void main(String args[])
+    {
+        /* Basic JFrame */
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("My app");
+        frame.setSize(400, 300);
+        frame.setLocation(600, 100);
+
+
+        //Create a panel with FlowLayout
+        JPanel panel = new JPanel(new FlowLayout());
+
+        //Radio buttons
+        JRadioButton radioBtn1 = new JRadioButton();
+        JRadioButton radioBtn2 = new JRadioButton();
+
+        //Adding radio buttons to panel
+        panel.add(radioBtn1);
+        panel.add(radioBtn2);
+
+        //Adding panel to frame
+        frame.add(panel);
+
+        //Dimensions
+        radioBtn1.setBounds(50, 50, 100, 50);
+        radioBtn2.setBounds(50, 100, 100, 50);
+
+        //Text
+        radioBtn1.setText("Male");
+        radioBtn2.setText("Female");
+
+        //Font
+        Font f = new Font("MV Boli", Font.PLAIN, 25);
+        radioBtn1.setFont(f);
+        radioBtn2.setFont(f);
+
+        //Button group
+        ButtonGroup group = new ButtonGroup();
+        group.add(radioBtn1);
+        group.add(radioBtn2);
+    }
+}
