7f1ae747dc832a3e09d4bb0f32192f7808cad3f7
Rishabh Shukla <rs515666@gmail.com>
2024-02-20 11:54:04 +0530

    Made some changes

--- Registration Form.java
+++ Registration Form.java
@@ -7,48 +7,73 @@ class RegistrationForm
     {
         JFrame frame = new JFrame();
         frame.setVisible(true);
+        frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Registration Form");
-        frame.setSize(600, 400);
-        frame.setLocation(0, 0);
+        frame.setSize(500, 450);
+        frame.setLocation(500, 100);
 
         /* NAME */
         JLabel nameLabel = new JLabel();
         frame.add(nameLabel);
         nameLabel.setText("Full name :");
         nameLabel.setFont(new Font("Arial", Font.ITALIC, 22));
-        nameLabel.setBounds(0, 0, 200, 50);
+        nameLabel.setBounds(20, 0, 200, 50);
 
-        JTextField nameField = new JTextField();
-        frame.add(nameField);
-        nameField.setText("name");
-        nameField.setFont(new Font("Arial", Font.PLAIN, 20));
-        nameField.setBounds(0, 50, 200, 50);
+        JTextField name = new JTextField();
+        frame.add(name);
+        name.setText("name");
+        name.setFont(new Font("Arial", Font.PLAIN, 20));
+        name.setBounds(20, 50, 200, 50);
+
 
         /* EMAIL */
         JLabel emailLabel = new JLabel();
         frame.add(emailLabel);
         emailLabel.setText("Email :");
         emailLabel.setFont(new Font("Arial", Font.ITALIC, 22));
-        emailLabel.setBounds(0, 100, 200, 50);
+        emailLabel.setBounds(20, 100, 200, 50);
 
-        JTextField emailField = new JTextField();
-        frame.add(emailField);
-        emailField.setText("email");
-        emailField.setFont(new Font("Arial", Font.PLAIN, 20));
-        emailField.setBounds(0, 150, 200, 50);
+        JTextField email = new JTextField();
+        frame.add(email);
+        email.setText("email");
+        email.setFont(new Font("Arial", Font.PLAIN, 20));
+        email.setBounds(20, 150, 200, 50);
+
 
         /* PASSWORD */
         JLabel passwordLabel = new JLabel();
         frame.add(passwordLabel);
         passwordLabel.setText("Password :");
         passwordLabel.setFont(new Font("Arial", Font.ITALIC, 22));
-        passwordLabel.setBounds(0, 200, 200, 50);
+        passwordLabel.setBounds(20, 200, 200, 50);
 
-        JTextField passwordField = new JTextField();
-        frame.add(passwordField);
-        passwordField.setText("password");
-        passwordField.setFont(new Font("Arial", Font.PLAIN, 20));
-        passwordField.setBounds(0, 250, 200, 50);
+        JTextField password = new JTextField();
+        frame.add(password);
+        password.setText("password");
+        password.setFont(new Font("Arial", Font.PLAIN, 20));
+        password.setBounds(20, 250, 200, 50);
+
+
+        /* GENDER */
+        JLabel genderLabel = new JLabel();
+        frame.add(genderLabel);
+        genderLabel.setText("Gender :");
+        genderLabel.setFont(new Font("Arial", Font.ITALIC, 22));
+        genderLabel.setBounds(20, 300, 200, 50);
+
+        JRadioButton male = new JRadioButton();
+        JRadioButton female = new JRadioButton();
+        frame.add(male);
+        frame.add(female);
+        male.setBounds(130, 300, 80, 50);
+        female.setBounds(210, 300, 100, 50);
+        male.setText("Male");
+        female.setText("Female");
+        male.setFont(new Font("Arial", Font.PLAIN, 20));
+        female.setFont(new Font("Arial", Font.PLAIN, 20));
+        ButtonGroup gender = new ButtonGroup();
+        gender.add(male);
+        gender.add(female);
     }
 }
