b3ed3949e656df1eef5ebf8f4cfe5bd098bfc1af
Rishabh Shukla <rs515666@gmail.com>
2024-02-26 01:42:26 +0530

    Removed a file

--- Button Action 3.java
+++ Button Action 3.java
@@ -2,8 +2,38 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+class RedClass implements ActionListener
+{
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        ButtonAction3.c.setBackground(Color.RED);
+    }
+}
+
+class GreenClass implements ActionListener
+{
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        ButtonAction3.c.setBackground(Color.GREEN);
+    }
+}
+
+class BlueClass implements ActionListener
+{
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        ButtonAction3.c.setBackground(Color.BLUE);
+    }
+}
+
+
 class ButtonAction3
 {
+    static Container c;
+
     public static void main(String args[])
     {
         /* Basic JFrame */
@@ -15,28 +45,38 @@ class ButtonAction3
         frame.setLocation(600, 100);
 
         //Container
-        Container c = frame.getContentPane();
+        c = frame.getContentPane();
         frame.setLayout(null);
 
-        //Button
-        JButton button = new JButton();
+        //Buttons
+        JButton button1 = new JButton();
+        JButton button2 = new JButton();
+        JButton button3 = new JButton();
 
-        //Adding button to frame
-        c.add(button);
+        //Adding buttons to frame
+        c.add(button1);
+        c.add(button2);
+        c.add(button3);
 
         //Text
-        button.setText("Click me");
+        button1.setText("Red");
+        button2.setText("Green");
+        button3.setText("Blue");
 
         //Bounds
-        button.setBounds(130, 100, 130, 30);
+        button1.setBounds(30, 100, 100, 30);
+        button2.setBounds(140, 100, 100, 30);
+        button3.setBounds(250, 100, 100, 30);
 
         //Font
         Font f = new Font("Arial", Font.BOLD, 22);
-        button.setFont(f);
+        button1.setFont(f);
+        button2.setFont(f);
+        button3.setFont(f);
 
-        //Adding action listener
-        button.addActionListener((e) -> {
-            c.setBackground(Color.RED);
-        });
+        //Adding action listeners
+        button1.addActionListener(new RedClass());
+        button2.addActionListener(new GreenClass());
+        button3.addActionListener(new BlueClass());
     }
 }
